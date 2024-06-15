02095a74beb5cad6781d95e8e4c68c70200d94b3
farshad-tavakoli <65855403+farshad-tavakoli@users.noreply.github.com>
2024-06-15 11:55:14 +0330

    adding templates and static

---
+++ core/templates/postcss.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+    "postcss-rtlcss": {
+      // invert ltr and rtl prefixes so that left and right would be natural in rtl
+      ltrPrefix: '[dir="rtl"]',
+      rtlPrefix: '[dir="ltr"]',
+    }
+
+  }
+}
